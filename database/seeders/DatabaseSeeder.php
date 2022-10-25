f138e66ad5b86f648c3236f3feb726b68bd84039
Heryani00 <heryanishyt1105@gmail.com>
2022-10-25 22:33:10 +0700

    menyelesaikan video database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Heryani',
+            'email' => 'heryanishyt1105@gmail.com',
+            'password' => bcrypt('yani11')
+        ]);
+
+        User::create([
+            'name' => 'Byun Baekhyun',
+            'email' => 'byun1105@gmail.com',
+            'password' => bcrypt('byun11')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione, maxime neque odit alias nulla repellendus sint iusto quo iste quae autem obcaecati suscipit enim vel aut pariatur. Veritatis pariatur vel, architecto non hic minus sunt totam nesciunt nobis consequuntur, earum facere id inventore saepe est molestiae, fugiat excepturi alias delectus ratione recusandae quis sapiente ipsum neque. Ullam saepe ad qui placeat, dolorum a quidem ratione aliquid, veritatis nobis, perspiciatis tenetur. Natus dicta reprehenderit consequuntur doloremque exercitationem architecto maiores non consequatur necessitatibus. Neque a illum ea aliquid explicabo natus amet corrupti, eaque, tenetur sapiente vel.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione, maxime neque odit alias nulla repellendus sint iusto quo iste quae autem obcaecati suscipit enim vel aut pariatur. Veritatis pariatur vel, architecto non hic minus sunt totam nesciunt nobis consequuntur, earum facere id inventore saepe est molestiae, fugiat excepturi alias delectus ratione recusandae quis sapiente ipsum neque. Ullam saepe ad qui placeat, dolorum a quidem ratione aliquid, veritatis nobis, perspiciatis tenetur. Natus dicta reprehenderit consequuntur doloremque exercitationem architecto maiores non consequatur necessitatibus. Neque a illum ea aliquid explicabo natus amet corrupti, eaque, tenetur sapiente vel.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione, maxime neque odit alias nulla repellendus sint iusto quo iste quae autem obcaecati suscipit enim vel aut pariatur. Veritatis pariatur vel, architecto non hic minus sunt totam nesciunt nobis consequuntur, earum facere id inventore saepe est molestiae, fugiat excepturi alias delectus ratione recusandae quis sapiente ipsum neque. Ullam saepe ad qui placeat, dolorum a quidem ratione aliquid, veritatis nobis, perspiciatis tenetur. Natus dicta reprehenderit consequuntur doloremque exercitationem architecto maiores non consequatur necessitatibus. Neque a illum ea aliquid explicabo natus amet corrupti, eaque, tenetur sapiente vel.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat, incidunt officia nobis magni ratione, maxime neque odit alias nulla repellendus sint iusto quo iste quae autem obcaecati suscipit enim vel aut pariatur. Veritatis pariatur vel, architecto non hic minus sunt totam nesciunt nobis consequuntur, earum facere id inventore saepe est molestiae, fugiat excepturi alias delectus ratione recusandae quis sapiente ipsum neque. Ullam saepe ad qui placeat, dolorum a quidem ratione aliquid, veritatis nobis, perspiciatis tenetur. Natus dicta reprehenderit consequuntur doloremque exercitationem architecto maiores non consequatur necessitatibus. Neque a illum ea aliquid explicabo natus amet corrupti, eaque, tenetur sapiente vel.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
