f7ea0231cabdb710529bf369af9a354e7737ded8
Heryani00 <heryanishyt1105@gmail.com>
2022-11-21 19:37:38 +0700

    menyelesaikan video Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
     {
 
         Paginator::useBootstrap();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'yanii';
+        });
     }
 }
